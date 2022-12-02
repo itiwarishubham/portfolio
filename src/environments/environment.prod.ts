f55407e3d9113be2c6cf77114dd53f87591e95ff
Shubham Tiwari <shubham.tiwari@wissen.com>
2022-12-03 00:08:03 +0530

    firebase auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBkkcVMEOYDZO_mIp-4A5TZuQGYCoXGBM8",
+    authDomain: "itiwarishubham-ef4dd.firebaseapp.com",
+    databaseURL: "https://itiwarishubham-ef4dd-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "itiwarishubham-ef4dd",
+    storageBucket: "itiwarishubham-ef4dd.appspot.com",
+    messagingSenderId: "153047323115",
+    appId: "1:153047323115:web:7f2b6d7248b2157ef9c9ae",
+    measurementId: "G-5Q67YFFRDN"
+  }
 };
