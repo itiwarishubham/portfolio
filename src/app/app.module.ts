678c101f929876935662424f0db5e3e5c7d9c3af
Shubham Tiwari <shubham.tiwari@wissen.com>
2022-12-02 10:00:14 +0530

    firebase auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,12 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -38,7 +44,12 @@ const routes: Routes = [
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule
   ],
   providers: [HttpClientModule,HttpClient],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBkkcVMEOYDZO_mIp-4A5TZuQGYCoXGBM8",
+    authDomain: "itiwarishubham-ef4dd.firebaseapp.com",
+    databaseURL: "https://itiwarishubham-ef4dd-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "itiwarishubham-ef4dd",
+    storageBucket: "itiwarishubham-ef4dd.appspot.com",
+    messagingSenderId: "153047323115",
+    appId: "1:153047323115:web:7f2b6d7248b2157ef9c9ae",
+    measurementId: "G-5Q67YFFRDN"
+  }
 };
 
 /*
